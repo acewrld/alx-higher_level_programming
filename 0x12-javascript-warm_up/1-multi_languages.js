7bcef1ef52157046dea2df8f35c4f53c802f4521
Ademola Olalere <olalerer2@gmail.com>
2023-03-13 14:48:00 -0700

    JavaScript is amazing

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,4 +2,5 @@
 
 //Script that print 3 lines"
 //
-const myVar = "C is fun"\n"Python is cool"\n"JavaScript is amazing"
+const myVar = "C is fun"\n"Python is cool"\n"JavaScript is amazing";
+console.log(myVar);
