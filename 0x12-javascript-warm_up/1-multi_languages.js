28551af9be49f8d1c4d286ae149e6ca929e54087
Ademola Olalere <olalerer2@gmail.com>
2023-03-15 09:16:07 -0700

    Scripts that prints multi lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,5 +2,5 @@
 //
 //Script that print 3 lines
 //
-const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
+const myVar = "C is fun\nPython is cool\nJavaScript is amazing";
 console.log(myVar);
